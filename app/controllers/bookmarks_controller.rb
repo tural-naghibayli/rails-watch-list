27c4f894a4d46be9378fd256859b577f24d9dc5c
Tural Naghibayli <tural.naghibayli@gmail.com>
2021-09-10 11:28:46 +0300

    Done root

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -38,11 +38,9 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to list_path(@bookmark.list)
-
   end
 
   private
